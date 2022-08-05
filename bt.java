82d6a21c7f2dbf82585dad7afb632cf2b7e3a24a
hoanglan <hoanglan.nguyen0208@gmail.com>
2022-08-06 00:48:06 +0900

    add file

---
+++ bt.java
@@ -0,0 +1,5 @@
+public class bt {
+    public static void main(String[] args) {
+        System.out.println("Hoang Lan");
+    }
+}
--- xinchao.java
+++
@@ -1,7 +0,0 @@
-public class xinchao {
-   public static void main(String[] args) {
-       System.out.println("Xin chao toi la Dung ");
-    }
-
-
-}
